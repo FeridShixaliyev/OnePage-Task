fe95eb52f3c223023e3652c6ee6fc596ff3a17ba
feridsixaliyev90 <feridsixaliyev90@gmail.com>
2022-06-07 11:54:39 +0400

    Add project files.

---
+++ OnePage/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnePage.DAL;
+using OnePage.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnePage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _sql;
+
+        public HomeController(AppDbContext sql)
+        {
+            _sql = sql;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
